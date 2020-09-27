753850cff552e28ff4ebd8fea6f8060bf98949e6
Prasanna Hegde <prasannanhegde@gmail.com>
2020-09-27 19:29:55 +0530

    unused variables removed

--- iq.js
+++ iq.js
@@ -1,24 +1,28 @@
-function iq(str){
-    let even = 0;
-    let odd = 0;
+function iq(str){ 
     let evenArr = [];
     let oddArr = [];
     numbers = str.split(" ");
 
     for(let i = 0; i < numbers.length; i++){
         if(numbers[i] % 2 == 0){
-            even += 1;
+
             evenArr.push(numbers[i]);
+
         }else{
-            odd += 1;
+            
             oddArr.push(numbers[i]);
+
         }        
     }
     
     if(evenArr.length == 1){
+
         return numbers.indexOf(evenArr[0]) + 1;
+
     }else{
+
         return numbers.indexOf(oddArr[0]) + 1;
+        
     }
 }
 
